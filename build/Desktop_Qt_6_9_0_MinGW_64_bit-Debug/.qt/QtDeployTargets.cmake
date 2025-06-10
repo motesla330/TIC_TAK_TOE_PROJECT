d4fd3d475143f0a653b3483db04e546cce3c5a34
Menna Assem <menafouda@gmail.com>
2025-06-11 02:06:33 +0300

    delete build files

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_FILE C:/Users/PC/OneDrive/Desktop/mannas/Tic Tac Toe/Tic_Tac_Test/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Tic_Tac_Test.exe)
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
--- out/build/x64-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_FILE C:/Users/PC/OneDrive/Desktop/mannas/Tic Tac Toe/Tic_Tac_Test/out/build/x64-Debug/Tic_Tac_Test.exe)
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Tic_Tac_Test_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
