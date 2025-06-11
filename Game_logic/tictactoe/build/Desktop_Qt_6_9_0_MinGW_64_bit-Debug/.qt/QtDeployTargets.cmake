cab371fff2275356b72303615365896593c6c03e
Menna Assem <menafouda@gmail.com>
2025-06-11 15:59:28 +0300

    Add full gamelogic folder

---
+++ Game_logic/tictactoe/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_tictactoe_FILE C:/Users/PC/OneDrive/Desktop/mannas/Tic Tac Toe/tictactoe/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/tictactoe.exe)
+set(__QT_DEPLOY_TARGET_tictactoe_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_tictactoe_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
