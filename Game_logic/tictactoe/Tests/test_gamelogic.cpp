eb4320c6302af4ae7402dd7aa45063efc3bf3762
Menna Assem <menafouda@gmail.com>
2025-06-14 22:12:46 +0300

    Update test_gamelogic.cpp

--- Game_logic/tictactoe/Tests/test_gamelogic.cpp
+++ Game_logic/tictactoe/Tests/test_gamelogic.cpp
@@ -1,9 +1,9 @@
-#include <gtest/gtest.h>
+/*#include <gtest/gtest.h>
 #include "gamelogic.h"  // Include your logic header
 
 TEST(GameLogicTest, TestInitialState) {
     GameLogic logic;
     EXPECT_EQ(logic.getCurrentPlayer(), Player::X); // example
-}
+}*/
 
 // Add more test cases...
