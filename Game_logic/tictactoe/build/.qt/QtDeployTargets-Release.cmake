c215097fef8890d98ae508f76c058cb272388bed
Menna Assem <menafouda@gmail.com>
2025-06-17 18:54:15 +0300

    update build

---
+++ Game_logic/tictactoe/build/.qt/QtDeployTargets-Release.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_tictactoe_FILE C:/Users/PC/OneDrive/Documents/GitHub/TIC_TAK_TOE_PROJECT/Game_logic/tictactoe/build/Release/tictactoe.exe)
+set(__QT_DEPLOY_TARGET_tictactoe_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_tictactoe_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
