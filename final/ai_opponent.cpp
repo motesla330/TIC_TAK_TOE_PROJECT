5eb842136249108d1506edc87575446f7fb19ea0
Menna Assem <menafouda@gmail.com>
2025-06-19 00:05:19 +0300

    apply bitmasking in the evaluate() function

--- final/ai_opponent.cpp
+++ final/ai_opponent.cpp
@@ -15,31 +15,30 @@ bool isMovesLeft(char board[3][3]) {
     return false;
 }
 
-int evaluate(char b[3][3]) {
-    for (int row = 0; row < 3; row++) {
-        if (b[row][0] == b[row][1] && b[row][1] == b[row][2]) {
-            if (b[row][0] == player) return +10;
-            else if (b[row][0] == opponent) return -10;
+int getBitmask(char board[3][3], char symbol) {
+    int mask = 0;
+    int pos = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == symbol)
+                mask |= (1 << pos);
+            pos++;
         }
     }
+    return mask;
+}
 
-    for (int col = 0; col < 3; col++) {
-        if (b[0][col] == b[1][col] && b[1][col] == b[2][col]) {
-            if (b[0][col] == player) return +10;
-            else if (b[0][col] == opponent) return -10;
-        }
+int evaluate(char board[3][3]) {
+    int playerMask = getBitmask(board, player);
+    int opponentMask = getBitmask(board, opponent);
+
+    for (int i = 0; i < 8; i++) {
+        int winMask = WIN_PATTERNS[i];
+        if ((playerMask & winMask) == winMask)
+            return +10;
+        if ((opponentMask & winMask) == winMask)
+            return -10;
     }
-
-    if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) {
-        if (b[0][0] == player) return +10;
-        else if (b[0][0] == opponent) return -10;
-    }
-
-    if (b[0][2] == b[1][1] && b[1][1] == b[2][0]) {
-        if (b[0][2] == player) return +10;
-        else if (b[0][2] == opponent) return -10;
-    }
-
     return 0;
 }
 
@@ -116,7 +115,7 @@ Move findMediumMove(char board[3][3]) {
     return (rand() % 2 == 0) ? findBestMove(board) : findRandomMove(board);
 }
 
-Move getAIMove(Board& boardObj, Difficulty diff) {
+Move getAIMove(Board& boardObj, Difficulty diff, char aiChar, char humanChar) {
     char tempBoard[3][3];
     const auto& grid = boardObj.getBoard();
 
@@ -124,7 +123,7 @@ Move getAIMove(Board& boardObj, Difficulty diff) {
         for (int j = 0; j < 3; ++j)
             tempBoard[i][j] = (grid[i][j] == ' ') ? '_' : grid[i][j];
 
-    // Set the symbols used by minimax
+  
     player = aiChar;
     opponent = humanChar;
 
--- final/ai_opponent.h
+++ final/ai_opponent.h
@@ -18,6 +18,17 @@ enum Difficulty {
     HARD
 };
 
+WIN_PATTERNS[8] = {
+    0b111000000,  // First row (cells 0-2)
+    0b000111000,  // Second row (cells 3-5)
+    0b000000111,  // Third row (cells 6-8)
+    0b100100100,  // First column (cells 0,3,6)
+    0b010010010,  // Second column (cells 1,4,7)
+    0b001001001,  // Third column (cells 2,5,8)
+    0b100010001,  // Diagonal (cells 0,4,8)
+    0b001010100   // Anti-diagonal (cells 2,4,6)
+};
+
 Move getAIMove(Board& board, Difficulty diff);
 
 #endif // AI_OPPONENT_H
