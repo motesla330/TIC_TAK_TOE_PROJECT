64bf90342a73579b330b0f918292db995c75df52
Menna Assem <menafouda@gmail.com>
2025-06-18 18:26:47 +0300

    update ai

--- AI_and_2Player/tictactoe/ai_opponent.h
+++ AI_and_2Player/tictactoe/ai_opponent.h
@@ -18,6 +18,16 @@ enum Difficulty {
     HARD
 };
 
+const int WINNING_MASKS[8] = {
+    0b000000111, // Row 0
+    0b000111000, // Row 1
+    0b111000000, // Row 2
+    0b001001001, // Col 0
+    0b010010010, // Col 1
+    0b100100100, // Col 2
+    0b100010001, // Diag
+    0b001010100  // Diag
+};
 // Updated declaration using Board class
 Move getAIMove(Board& board, Difficulty diff, char aiPlayer, char humanPlayer);
 
